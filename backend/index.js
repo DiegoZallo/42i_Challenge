cf6a7ae2b3ff987b8eb9ea4ec946aa75f600e710
diegozallocco <diegozallocco@hotmail.com>
2024-02-26 15:14:12 -0300

    solving cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app.js');
   const port = process.env.PORT || 3001
 
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log(`Server listening at PORT:${port}`); // eslint-disable-line no-console
   });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const server = express();
 const routes = require('./routes/index.js');
+const cors = require("cors");
 
 server.use(express.json());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  next();
-});
+server.use(cors());
 
 server.use('/', routes);
 
